ebd10d12cbef6b4309a4cdabeb537f0f2e2695e9
Mortaza1o <zmoner680@gmail.com>
2024-07-31 02:01:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-const gameArea = document.getElementById('gameArea');
-let score = 0;
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
 
-function createStar() {
-    const star = document.createElement('div');
-    star.classList.add('star');
-
-    // تعيين موضع النجمة بشكل عشوائي في أعلى الشاشة
-    const startPosition = Math.floor(Math.random() * gameArea.offsetWidth);
-    star.style.left = `${startPosition}px`;
-
-    // تعيين مدة الحركة بشكل عشوائي بين 3 و 6 ثوانٍ
-    const duration = Math.random() * 3 + 3;
-    star.style.animationDuration = `${duration}s`;
-
-    star.addEventListener('click', () => {
-        score++;
-        document.getElementById('score').innerText = score;
-        star.remove();
-    });
-
-    star.addEventListener('animationend', () => {
-        star.remove();
-    });
-
-    gameArea.appendChild(star);
-}
-
-function startGame() {
-    setInterval(createStar, 1000);
-}
-
-startGame();
+    if (username === 'مرتضى' && password === 'مريم') {
+        document.querySelector('.heart').style.display = 'none';
+        document.getElementById('images').style.display = 'block';
+    } else {
+        alert('Invalid username or password.');
+    }
+});
